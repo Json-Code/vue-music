f7157727b4497baaab87f4b1f9352a9be1074559
Json-Code <596344059@qq.com>
2018-11-08 22:58:32 +0800

    歌单推荐页面开发完成

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,11 +1,73 @@
 <template>
   <div class="rank" ref="rank">
-    我是rank页面
+    <scroll class="toplist" ref="toplist">
+      <ul>
+        <li class="item" v-for="(item, index) in topList" :key="index" @click="selectItem(item)">
+          <div class="icon">
+            <img width="100" height="100" v-lazy="item.picUrl"/>
+          </div>
+          <ul class="songlist">
+            <li class="song" v-for="(song, index) in item.songList" :key="index">
+              <span>{{index + 1}}</span>
+              <span>{{song.songname}}--{{song.singgername}}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div class="loading-container" v-show="!topList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import {getTopList} from 'api/rank'
+  import {ERR_OK} from 'api/config'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import {playlistMixin} from 'common/js/mixin'
+  import {mapMutations} from 'vuex'
+
+  export default {
+    mixins: [playlistMixin],
+    created() {
+      this._getTopList()
+    },
+    data() {
+      return {
+        topList: []
+      }
+    },
+    methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length ? '60px' : ''
+        this.$refs.rank.style.bottom = bottom
+        this.$refs.toplist.refresh()
+      },
+      selectItem(item) {
+        this.$router.push({
+          path: `/rank/${item.id}`
+        })
+        this.setTopList(item)
+      },
+      _getTopList() {
+        getTopList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.topList = res.data.topList
+          }
+        })
+      },
+      ...mapMutations({
+        setTopList: 'SET_TOP_LIST'
+      })
+    },
+    components: {
+      Scroll,
+      Loading
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
